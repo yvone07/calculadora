e17837fa4e3800a9a3822f7dab774ef4ff52076e
yvone07 <yvoneelisadasilva1@gmail.com>
2024-10-26 11:50:02 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+numero1 = float(input("Digite o primeiro numero: "))
+numero2 = float(input("Digite o segundo numero: "))
+
+soma= numero1 + numero2 
+subtracao = numero1 - numero2
+divisao = numero1 / numero2 
+multiplicacao = numero1 * numero2 
+
+print("Soma: ", soma)
+print("Subtração: ", subtracao)
+print("Divisão: ", divisao)
+print("Multiplicação:", multiplicacao)
